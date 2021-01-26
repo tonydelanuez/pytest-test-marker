5dfc3f3ae0ea0000017af246ebd6c4d75010c3b0
Tony De La Nuez <tony.delanuez@gmail.com>
2021-01-26 17:40:56 -0600

    remove testdata generation code

--- tests/test_mark.py
+++ tests/test_mark.py
@@ -1,11 +1,14 @@
+import pytest
 import uuid
 
-from unittest.mock import Mock
-from pytest_test_marker.mark import CollectionModifier, parse_nodeid
 
+from pytest_test_marker.mark import CollectionModifier, MarkDefinition, parse_nodeid
+
+
+test_marker_name = 'example_marker'
 
 test_dict = {
-    'example_marker': {
+    test_marker_name: {
         'modules': ['src/test2.py'],
         'classes': ['src/test0.py::ExampleTestCase1'],
         'functions': ['src/test1.py::ExampleTestCase2::ex_test_func_2']
@@ -20,17 +23,6 @@ def gen_test_id(include_class=True, include_func=True):
     if include_func:
         testid += '::test_{}'.format(str(uuid.uuid4())[:8])
     return testid
-
-
-def create_test_node():
-    node = Mock()
-    test_id = gen_test_id()
-    node.nodeid.side_effect = test_id
-    return node
-
-
-def create_multiple_test_nodes(n=10):
-    return [create_test_node() for _ in range(n)]
 
 
 def test_parse_nodeid_module_only():
@@ -58,3 +50,27 @@ def test_parse_nodeid_full_path():
     assert parsed.module_path == "src/test.py"
     assert parsed.class_path == "src/test.py::ExampleTestCase"
     assert parsed.func_path == "src/test.py::ExampleTestCase::test_foo"
+
+
+@pytest.mark.parametrize("test_path,expected",
+                         [("src/test2.py", True),
+                          ("src/WRONG.py", False)])
+def test_markdefinition_in_marked_module(test_path, expected):
+    md = MarkDefinition(test_marker_name, test_dict[test_marker_name])
+    assert md.in_marked_module(test_path) == expected
+
+
+@pytest.mark.parametrize("test_path,expected",
+                         [("src/test0.py::ExampleTestCase1", True),
+                          ("src/test2.py::UnmarkedTestCase", False)])
+def test_markdefinition_in_marked_class(test_path, expected):
+    md = MarkDefinition(test_marker_name, test_dict[test_marker_name])
+    assert md.in_marked_class(test_path) == expected
+
+
+@pytest.mark.parametrize("test_path,expected",
+                         [("src/test1.py::ExampleTestCase2::ex_test_func_2", True),
+                          ("src/test2.py::UnmarkedTestCase::test_foo", False)])
+def test_markdefinition_in_marked_function(test_path, expected):
+    md = MarkDefinition(test_marker_name, test_dict[test_marker_name])
+    assert md.in_marked_func(test_path) == expected
