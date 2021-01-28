2017994b1f56a0cb09727927ea06ed9d5b34bfa4
Tony De La Nuez <tony.delanuez@gmail.com>
2021-01-27 21:48:06 -0600

    Fix broken test using full filepath when working in testdir

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,18 +1,21 @@
-import os
-
 pytest_plugins = ['pytester']
 
-def test_mypytest_test(testdir):
+
+def test_mark_test_should_run(testdir):
     pytestfile = testdir.makepyfile("""
         def test_x(): pass
         def test_y(): pass
         def test_z(): pass
     """)
+
     markfile = testdir.makefile('.yml', """
     example_mark:
         functions:
-            {}::test_x
-    """.format(str(pytestfile)))
+            - {}::test_x
+    """.format(pytestfile.basename))
 
-    result = testdir.runpytest_subprocess('--mark-file', str(markfile))
+    result = testdir.runpytest('--mark-file',
+                               str(markfile),
+                               '-m',
+                               'example_mark')
     result.assert_outcomes(passed=1)
