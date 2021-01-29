36d02f791983ec39ffefc10839f62803c835a558
Tony De La Nuez <tony.delanuez@gmail.com>
2021-01-29 17:59:25 -0600

    Add tests for module and class marking

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,7 +1,7 @@
 pytest_plugins = ['pytester']
 
 
-def test_mark_test_should_run(testdir):
+def test_mark_func_should_run(testdir):
     pytestfile = testdir.makepyfile("""
         def test_x(): pass
         def test_y(): pass
@@ -19,3 +19,53 @@ def test_mark_test_should_run(testdir):
                                '-m',
                                'example_mark')
     result.assert_outcomes(passed=1)
+
+
+def test_mark_class_should_run(testdir):
+    pytestfile = testdir.makepyfile("""
+
+    def test_x(): pass
+
+    class TestFoo:
+        def test_y(self): pass
+
+        def test_z(self): pass
+
+    """)
+
+    markfile = testdir.makefile('.yml', """
+    example_mark:
+        classes:
+            - {}::TestFoo
+    """.format(pytestfile.basename))
+
+    result = testdir.runpytest('--mark-file',
+                               str(markfile),
+                               '-m',
+                               'example_mark')
+    result.assert_outcomes(passed=2)
+
+
+def test_mark_module_should_run(testdir):
+    pytestfile = testdir.makepyfile("""
+        def test_x(): pass
+        def test_y(): pass
+        def test_z(): pass
+    """)
+    testdir.makepyfile("""
+        def test_x(): pass
+        def test_y(): pass
+        def test_z(): pass
+    """)
+
+    markfile = testdir.makefile('.yml', """
+    example_mark:
+        modules:
+            - {}
+    """.format(pytestfile.basename))
+
+    result = testdir.runpytest('--mark-file',
+                               str(markfile),
+                               '-m',
+                               'example_mark')
+    result.assert_outcomes(passed=3)
